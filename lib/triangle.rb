c47eee7bb8726bed12694249cb442a95375cf234
Michael Davis <jrmdd1496@gmail.com>
2022-10-05 11:04:14 -0500

    commit tree 5cfe92cdd36331881c48478c0decd903bf0af7c1

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize (a ,b ,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if (((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a))
+      raise TriangleError
+    elsif (a <= 0 || b <= 0 || c <= 0)
+      raise TriangleError
+    else
+      if (a == b && b == c)
+        :equilateral
+      elsif ((a == b && b != c) || (a == c && b != c) || (b == c && a != c))
+        :isosceles
+      elsif (a != b && b != c && c != a)
+        :scalene  
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+binding.pry
